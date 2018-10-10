2fabd302d2b5d99daefeeff7a4fe3911908afb78
Ubuntu <ubuntu@ip-172-31-27-95.ap-southeast-1.compute.internal>
2018-10-10 01:05:45 +0000

    prod

--- env/env_var.js
+++ env/env_var.js
@@ -1,6 +1,6 @@
 module.exports = {
-      file: "/Users/karanahuja/Workspace/interview_digix/digix_solidity/build/contracts/",
-      source: "/Users/karanahuja/Workspace/interview_digix/digix_solidity/build/",
+      file: "/home/ubuntu/ERC20-ERC721/build/contracts/",
+      source: "/home/ubuntu/ERC20-ERC721/build/",
       hodl_contract_address: "0xa678aee86c42180bebcb922ea0f7a414af9a5cee",
       dummy_erc20_address: "0xaa6a79362bdba736ab933e0db5377e661ff4da6a",
       demurrage_contract_address: "0x3a2f923e8d93572599a35070c6a07d082b1958e3",
